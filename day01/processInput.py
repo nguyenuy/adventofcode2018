e15a4fb261c7a37fc61084e0bf6ff839e98af1e1
Uy Nguyen <nguyenuy@gmail.com>
2018-12-01 17:25:05 -0500

    Finish part 1 day 1

--- day01/processInput.py
+++ day01/processInput.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
+file_name = "input.txt"
+starting_input = 0
+
 def process_input():
-    print("hello")
+    running_counter = starting_input
+    with open (file_name, "r") as input_file:
+        for line in input_file:
+            value = get_absolute_value(line)
+            if is_negative(line):
+                running_counter = running_counter - value
+            else:
+                running_counter = running_counter + value
+    
+    print("The value is %d", running_counter)
+
+def is_negative(number):
+    sign = number[0]
+    return sign is '-'
+
+def get_absolute_value(number):
+    return int(number[1:])    
 
 if __name__ == '__main__':
     print("Hello there")
+    process_input()
