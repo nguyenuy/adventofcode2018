292dde43d417d517ccdf5bc0b54c342debb92095
Uy Nguyen <nguyenuy@gmail.com>
2018-12-02 21:36:19 -0500

    Finish day 1

--- day01/processInput.py
+++ day01/processInput.py
@@ -2,18 +2,27 @@
 
 file_name = "input.txt"
 starting_input = 0
+resulting_frequencies = set() # Initialize list
 
 def process_input():
     running_counter = starting_input
-    with open (file_name, "r") as input_file:
-        for line in input_file:
-            value = get_absolute_value(line)
-            if is_negative(line):
-                running_counter = running_counter - value
-            else:
-                running_counter = running_counter + value
-    
-    print("The value is %d", running_counter)
+    resulting_frequencies.add(starting_input)
+    while True:
+        with open (file_name, "r") as input_file:
+            for line in input_file:
+                value = get_absolute_value(line)
+                if is_negative(line):
+                    running_counter = running_counter - value
+                else:
+                    running_counter = running_counter + value
+                
+                if running_counter in resulting_frequencies:
+                    print("We found you: %d" % running_counter)
+                    exit()
+                else:
+                    resulting_frequencies.add(running_counter)
+        
+        #print("The value is %d", running_counter)
 
 def is_negative(number):
     sign = number[0]
