fd09917e00e06a62d31dbf4ec929dde6918cc39f
Uy Nguyen <nguyenuy@gmail.com>
2018-12-09 23:06:34 -0500

    Finish day07

--- day07/day07.py
+++ day07/day07.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import re
 from collections import defaultdict
+import collections
 
 day = 7 
-file_name = "input2.txt"
+file_name = "input.txt"
 
 def get_input():
     with open (file_name, "r") as input_file:
@@ -23,10 +24,59 @@ def process_input():
         available_letters.add(m.group(2))
         available_letters.add(m.group(1))
 
+    for i in available_letters:
+        if i not in requires:
+            requires[i] = set()
+
     sorted_letters = sorted(available_letters)
-    while sorted_letters != set():
-        break
-     
+    sorted_requires = collections.OrderedDict(sorted(requires.items()))
+    output = []
+    while bool(sorted_requires):
+        for k,v in sorted_requires.items():
+            if v - set(output) == set():
+                output.append(k)
+                sorted_requires.pop(k)
+                break
+
+    ## Part 1
+    print("".join(output))
+    
+    ## Edit these values
+    base_time = 60
+    current_time = 0 
+    number_workers = 5
+   
+    letters_processed = set()
+    letters_to_process = output.copy()
+    workers_processing = {}
+    while set(letters_processed) != set(output):
+        # Check if workers have work expired
+        workers_to_remove = set()
+        for k,v in workers_processing.items():
+            if v == current_time:
+                letters_processed.add(k)
+                letters_to_process.remove(k)
+                workers_to_remove.add(k)
+        
+        for i in workers_to_remove:
+            workers_processing.pop(i)
+
+        # Add work to workers
+        for i in letters_to_process:
+            if len(workers_processing) < number_workers:
+                if i not in workers_processing:
+                    if (requires[i] - letters_processed) == set():
+                        workers_processing[i] = current_time + base_time + ord(i) - ord('A') + 1
+       
+        current_time += 1
+    
+    print(current_time - 1) #counter counts up one more time
+        
+
+        
+
+
+
 
 if __name__ == '__main__':
     print("Executing day %d code...." % day)
