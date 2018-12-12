75c1fe54b201201aa5e7ae0c31560a7a45c08956
Uy Nguyen <nguyenuy@gmail.com>
2018-12-12 18:22:37 -0500

    Day 8 complete

--- day08/day08.py
+++ day08/day08.py
@@ -2,36 +2,66 @@
 import re
 from collections import defaultdict
 import collections
+import sys
+
+sys.setrecursionlimit(1000000)
 
 day = 7 
-file_name = "input2.txt"
+file_name = "input.txt"
 
 def get_input():
     with open (file_name, "r") as input_file:
         input = input_file.read().splitlines()
     return input
     
-def process_input():
-    print("Running problem 1")
-    input = get_input()
-    entries = input[0].split()
+def process_input(tree):
+    #print("here")
+    #print(tree)
+    root_nodes, root_metadata = tree[:2]
+
+    total_metadata = 0
+    remaining_tree = tree[2:]
     
-    root_header = entries[0:2]
-    root_nodes = root_header[0]
-    root_metadata = root_header[1]
-
-    metadata_entries = []
-    for i in range(0, len(entries)):
-        pass
-
+    # Terminating condition
+    total_metadata_sum = 0
+    for i in range(0, root_nodes):
+        #print("here2")
+        #print(remaining_tree)
+        metadata_sum, remaining_tree = process_input(remaining_tree)
+        total_metadata += metadata_sum
     
+    total_metadata += sum(remaining_tree[:root_metadata])
 
-        
+    return total_metadata, remaining_tree[root_metadata:]
 
+def parse(data):
+    children, metas = data[:2]
+    data = data[2:]
+    scores = []
+    totals = 0
 
+    for i in range(children):
+        total, data = parse(data)
+        totals += total
+
+    totals += sum(data[:metas])
+
+    return (totals, data[metas:])
 
 
 if __name__ == '__main__':
     print("Executing day %d code...." % day)
-    process_input()
+    print("Running problem 1")
+    
+    input = get_input()
+    entries = input[0].split()
+    entries = [int(i) for i in entries]
+    x = process_input(entries)
+    print(x[0])
+    print(process_input([0, 3, 1, 1, 1]))
+
+    total, remaining = parse(entries)
+
+    print('part 1:', total, remaining)
+ 
 
